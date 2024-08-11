d616e40b20312ffcb6159e53b49353516a7cb08c
Jatin Suyal <jatin16suyal01@gmail.com>
2024-08-12 00:30:04 +0530

    Cors error try1

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,8 +1,28 @@
+// export default [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+import { Strapi } from '@strapi/strapi';
+
 export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://cardgamestrapi-production.up.railway.app', 'https://cardgamestrapi-production.up.railway.app'], // Replace with your Unity app URLs
+      headers: '*',
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
